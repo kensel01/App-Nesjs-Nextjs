3ff2fd67c1fe5e932ba929a5dd49d826bc933a87
Kensel Mckillop <kensel.mckillop@gmail.com>
2024-12-15 17:05:14 -0300

    Next NextAuth

---
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "email", type: "text", placeholder: "test@test.com" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials, req) {
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
+            }
+        })
+    ]
+        
+});
+
+export { handler as GET, handler as POST };
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import ButtonAuth from "../../components/ButtonAuth";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ButtonAuth />
+    </div>
+  );
+};
+
+export default HomePage;
