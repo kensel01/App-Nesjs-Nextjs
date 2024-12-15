262a369caff21dee6250777754d3ac96c99157ac
Kensel Mckillop <kensel.mckillop@gmail.com>
2024-12-14 22:15:39 -0300

    Document Api

---
+++ Backend/src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstans } from './constants/jwt.constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {expiresIn: '1h'},
+        global: true,
+      }),
+      inject: [ConfigService]
+    })
+    //JwtModule.register({
+    //  global: true,
+    //  secret: jwtConstans.secret,
+    //  signOptions: {expiresIn: '1h'},
+    //})
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [JwtModule],
+})
+export class AuthModule {}
